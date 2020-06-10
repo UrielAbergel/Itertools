f2ff5c2a64d812bd833db5f4af47259cf9ac3b47
urielabergel <myfliper2@gmail.com>
2020-06-10 11:52:24 +0300

    start project

---
+++ cmake-build-debug/CMakeFiles/IteratorStruct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IteratorStruct.dir/main.cpp.o"
+  "IteratorStruct"
+  "IteratorStruct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IteratorStruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/IteratorStruct.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IteratorStruct.
+# This may be replaced when dependencies are built.
